2e776def8c5aac9be0965aa73fdd51903d04c846
Graziele Justina <165972958+grazielejustina@users.noreply.github.com>
2024-08-02 21:57:43 -0300

    Carregamento de dados
    Implemente a funcionalidade para carregar um conjunto de dados em formato CSV ou outro formato simples suportado pelo Python

---
+++ codigopython.py
@@ -0,0 +1,21 @@
+#Carregamento de Dados: Implemente a funcionalidade para carregar um conjunto de dados em formato CSV ou outro formato simples suportado pelo Python.
+
+#importar a biblioteca Pandas
+import pandas as pd
+
+#definição da função
+def carregar_csv(caminho_arquivo):
+
+#tentando ler o arquivo CSV por meio de codificações 
+    codificacoes = ['utf-8', 'latin1', 'windows-1252']
+    for encoding in codificacoes:
+        try:
+            return pd.read_csv(caminho_arquivo, encoding=encoding, parse_dates=["Data"])
+        except UnicodeDecodeError:
+            continue
+ 
+# Caminho para o arquivo CSV
+caminho_arquivo = 'C:\\Users\\grazi\\Downloads\\codes\\Análise de Dados Vendas de Cursos Online.csv'
+
+# Carregar o DataFrame
+df = carregar_csv(caminho_arquivo)
